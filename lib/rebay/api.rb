e14ffed74ced05e56556dc98061da0e3386039ca
Richard Cao <richard@mtggoldfish.com>
2015-11-05 15:50:13 -0800

    Add http timeouts

--- lib/rebay/api.rb
+++ lib/rebay/api.rb
@@ -41,7 +41,12 @@ module Rebay
     protected
     
     def get_json_response(url)
-      Rebay::Response.new(JSON.parse(Net::HTTP.get_response(URI.parse(url)).body))
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.read_timeout = 0.5
+      http.open_timeout = 0.5
+      response = http.start() {|http| http.get(url.path) }
+      Rebay::Response.new(JSON.parse(response.body))
     end
 
     def build_rest_payload(params)
