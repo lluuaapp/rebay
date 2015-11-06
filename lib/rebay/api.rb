aff27f8466c194327d829271c845fc93cf45eb42
Richard Cao <richard@mtggoldfish.com>
2015-11-05 16:09:35 -0800

    Fix call.

--- lib/rebay/api.rb
+++ lib/rebay/api.rb
@@ -45,7 +45,7 @@ module Rebay
       http = Net::HTTP.new(uri.host, uri.port)
       http.read_timeout = 0.5
       http.open_timeout = 0.5
-      response = http.start() {|http| http.get(uri.path) }
+      response = http.start() {|http| http.request_get(uri.request_uri) }
       Rebay::Response.new(JSON.parse(response.body))
     end
 
